1fc5de8a8926a19ee5b8664a4d6079132a3e70e3
pauhp96 <78864385+pauhp96@users.noreply.github.com>
2021-06-13 14:47:49 -0500

    Create product.js

---
+++ Pau/Proyecto desarrollasoftware/models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+//Generar nuestro propio módulo
+
+module.exports = mongoose.model('Product', new mongoose.Schema({
+    sku: String,
+    name: String,
+    description: String,
+    stock: Number,
+    price: Number,
+    images:Array
+}), 'Products');
