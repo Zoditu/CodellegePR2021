33e6a4629031909d44cec28471588afd1dab5a64
KMCF <kevin.martin@softtek.com>
2021-06-15 11:07:24 -0500

    Filtro de búsqueda

--- Kevin Martin Del Campo/desarolla.Store/public/Store/src/components/catalog/catalog.ts
+++ Kevin Martin Del Campo/desarolla.Store/public/Store/src/components/catalog/catalog.ts
@@ -23,11 +23,12 @@ export class CatalogComponent implements OnInit {
   }
 
   GetProducts() {
+
     var self = this;
     Singleton.GetInstance().ShowLoader();
     $.ajax({
       type: "GET",
-      url: 'http://localhost:666/products/all',
+      url: `http://localhost:666/products/search?name=${this.searchFilters.name}&price=0,${this.searchFilters.price}&stock=${this.searchFilters.stock ? 'true' : ''}`,
       success: function (res: any) {
         //console.log(self.products); //null
         self.products = res;
@@ -60,5 +61,30 @@ export class CatalogComponent implements OnInit {
     });
   }
 
-  products = null;
+  products = new Array;
+  searchFilters = {
+    name: '',
+    price: 100000,
+    stock: false
+  }
+
+  UpdateName(element: any) {
+    this.searchFilters.name = element.value;
+  }
+
+  UpdatePrice(element: any) {
+    this.searchFilters.price = element.value;
+  }
+
+  UpdateStock(element: any) {
+    this.searchFilters.stock = element.checked;
+    this.GetProducts();
+  }
+
+  CheckKey(event:any) {
+    //Si es enter
+    if(event.keyCode === 13) {
+      this.GetProducts();
+    }
+  }
 }
